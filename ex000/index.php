33201755fb1c4891daa2e34273abf7e164215abe
Ayanara <aya.nathane@gmail.com>
2023-11-16 18:21:12 -0300

    primeiro commit

---
+++ ex000/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP</title>
+</head>
+<body>
+  <h1>
+    <?php
+    print "Olá, Mundo! \u{1F30E}";
+    ?>
+  </h1>
+
+  <p>Testando comando com PHP</p>
+</body>
+</html>
---
+++ ex001/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Curso PHP - Exercicio 001 </title>
+</head>
+<body>
+
+<h1>Dados do Servidor</h1>
+
+<?php 
+  phpinfo();
+?>
+  
+</body>
+</html>
