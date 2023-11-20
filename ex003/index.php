afd99b8a3da3e8a73e38d3a5456110a17da02138
Ayanara <aya.nathane@gmail.com>
2023-11-20 09:32:53 -0300

    cria as funções hora do almoço e hora da janta

--- ex003/index.php
+++ ex003/index.php
@@ -9,8 +9,30 @@
   <h1>Exemplo de PHP</h1>
   <?php
     date_default_timezone_set("America/Sao_Paulo"); // GMT-3
+
+    function hora_almoco() {
+      $hora_atual = date("G:i:s");
+      if ($hora_atual == "12:00:00") {
+        return "É hora do almoço!";
+      } else {
+        return "A espera do proximo rango";
+      }
+    }
+
+    function hora_janta() {
+      $hora_atual = date("G:i:s");
+      if ($hora_atual == "18:00:00") {
+        return "É hora da janta!";
+      } else {
+        return "A espera do proximo rango";
+      }
+    }
+
     echo "Hoje é " . date("l") . ", " . date("d/m/Y"); // Usa o 'l' para o nome completo do dia
     echo " e a hora atual é " . date("G:i:s");
+
+    echo hora_almoco();
+    echo hora_janta();
   ?>
 </body>
 </html>
